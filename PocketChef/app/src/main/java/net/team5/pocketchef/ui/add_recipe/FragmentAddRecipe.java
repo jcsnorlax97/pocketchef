a3e9e78ab89b5bb4de17807857021d063fda5349
Justin <justin.choi.developer@gmail.com>
2020-02-04 14:06:55 -0600

    Add a fragment for feature2 (the fragment can be accessed through the bottom navbar); Add name and categories input entries in this fragment

---
+++ PocketChef/app/src/main/java/net/team5/pocketchef/ui/add_recipe/FragmentAddRecipe.java
@@ -0,0 +1,51 @@
+package net.team5.pocketchef.ui.add_recipe;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import net.team5.pocketchef.R;
+
+public class FragmentAddRecipe extends Fragment {
+
+    // --- variables ---
+    Button btnAddRecipe;
+    EditText etRecipeName;
+    EditText etRecipeCategory;
+    String recipeName;
+    String recipeCategory;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_add_recipe, container, false); // create view based on fragment_add_recipe.xmll in layout
+
+        // link view to variables
+        etRecipeName = view.findViewById(R.id.etRecipeName);
+        etRecipeCategory = view.findViewById(R.id.etRecipeCategory);
+        btnAddRecipe = view.findViewById(R.id.btnAddRecipe);
+
+        // add event handler for onClick event
+        btnAddRecipe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                recipeName = etRecipeName.getText().toString();
+                recipeCategory = etRecipeCategory.getText().toString();
+
+                // show what has been entered
+                Toast.makeText(getContext(), recipeName + " " + recipeCategory + " is entered.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        return view;
+    }
+}
