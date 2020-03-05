7a500ed446ef6663af30119c0155699b81690d9c
Jaskaran Singh <jas@Jaskarans-MacBook-Pro.local>
2020-03-05 14:14:10 -0600

    change

--- PocketChef/app/src/main/java/net/team5/pocketchef/MainActivity.java
+++ PocketChef/app/src/main/java/net/team5/pocketchef/MainActivity.java
@@ -1,6 +1,9 @@
 package net.team5.pocketchef;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -11,6 +14,7 @@ import androidx.navigation.ui.NavigationUI;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+
+
 
     }
 
