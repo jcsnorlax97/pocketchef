b1c9af4b067f26cdc83c9b6648be5897da066792
Justin <justin.choi.developer@gmail.com>
2020-04-06 15:48:51 -0500

    Remove unused and commented codes, Remove useless comments

--- PocketChef/app/src/main/java/net/team5/pocketchef/ui/add_recipe/FragmentAddRecipe.java
+++ PocketChef/app/src/main/java/net/team5/pocketchef/ui/add_recipe/FragmentAddRecipe.java
@@ -43,7 +43,6 @@ public class FragmentAddRecipe extends Fragment {
 
     // --- variables for ingredients ---
     MaterialButton btnAddIngredient;
-    MaterialButton btnCheckAllIngredients;
     ChipGroup chipGroup;
     Chip chipItem;
     TextInputEditText tietIngredient;
@@ -80,7 +79,6 @@ public class FragmentAddRecipe extends Fragment {
 
         // --- ingredients ---
         btnAddIngredient = view.findViewById(R.id.btnAddIngredient);
-//        btnCheckAllIngredients = view.findViewById(R.id.btnCheckAllIngredients);
         chipGroup = view.findViewById(R.id.chipGroup);
         tietIngredient = view.findViewById(R.id.tietIngredient);
 
@@ -115,10 +113,10 @@ public class FragmentAddRecipe extends Fragment {
                     return;
                 }
 
-                LayoutInflater inflater = LayoutInflater.from(getContext()); // (Not sure if this is the correct way to do it.)
+                LayoutInflater inflater = LayoutInflater.from(getContext());
 
                 // create view for a chipItem
-                chipItem = (Chip) inflater.inflate(R.layout.chip_item, null, false); // (Not sure if this is the correct way to do it.)
+                chipItem = (Chip) inflater.inflate(R.layout.chip_item, null, false);
                 chipItem.setText(ingredient); // replace default text with the current one.
 
                 // add event handler for the close icon, i.e. when the close icon in the tag is clicked, execute the following.
@@ -140,20 +138,6 @@ public class FragmentAddRecipe extends Fragment {
                 vScrollView.fullScroll(ScrollView.FOCUS_DOWN);
             }
         });
-
-//        // When 'btnCheckAllIngredients' button is clicked, it checks all ingredients that have been added.
-//        btnCheckAllIngredients.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//                // iterate over each chip in the group, get the chip item name, and append to result.
-//                for(int i=0; i < chipGroup.getChildCount(); i++) {
-//                    Chip chip = (Chip) chipGroup.getChildAt(i);
-//                    chip.setChecked(true);
-//                }
-//
-//            }
-//        });
 
         // When 'btnAddRecipe' button is clicked, display what have been entered.
         // PS:  At the end, it will pass the data to the business layer to add the recipe information
@@ -219,7 +203,7 @@ public class FragmentAddRecipe extends Fragment {
                 }
 
                 // --- ingredients ---
-                StringBuilder result = new StringBuilder(""); // (just for testing; should remove it later)
+                StringBuilder result = new StringBuilder(); // for printing what has been entered
 
                 // iterate over each chip in the chip group, get the chip item name, and add to the ingredientList
                 for(int i=0; i < chipGroup.getChildCount(); i++) {
@@ -231,6 +215,7 @@ public class FragmentAddRecipe extends Fragment {
                     if (MainActivity.manager.getIngredient(ingredient.getIngredientName()) == null)
                         MainActivity.manager.addIngredient(ingredient);
 
+                    // add to the ingredient list, which will be part of the new RecipeObject.
                     newRecipeIngredients.add(ingredient);
 
                     // append string for testing
